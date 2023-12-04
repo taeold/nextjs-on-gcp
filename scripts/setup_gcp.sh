4b962a7d319d8275ad48f59241d50159a6c0cc5d
Daniel Lee <taeold@gmail.com>
2023-12-04 09:29:14 +0900

    Initialize Firestore.

--- scripts/setup_gcp.sh
+++ scripts/setup_gcp.sh
@@ -13,6 +13,8 @@ usage() {
 
 exe() { echo "\$ $@" ; "$@" ; }
 
+qexe() { "$@" > /dev/null 2>&1 ; }
+
 # Parse the command line arguments
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -44,40 +46,77 @@ fi
 
 echo "Setting up project $PROJECT_ID in region $REGION"
 
-echo
 echo "========================================================="
-echo "Enabling required Google Cloud Platfrom APIs"
+echo "Checking Billing Info"
 echo "========================================================="
-exe gcloud services enable run.googleapis.com --project $PROJECT_ID
-exe gcloud services enable cloudbuild.googleapis.com --project $PROJECT_ID
+if ! gcloud billing projects describe $PROJECT_ID | grep "billingEnabled: true" > /dev/null 2>&1; then
+    echo "Error: Billing is not enabled for project $PROJECT_ID"
+    echo "Please enable billing for this project and try again."
+    echo "https://cloud.google.com/billing/docs/how-to/modify-project"
+    exit 1
+fi
+echo "Billing is enabled for project $PROJECT_ID"
+
 
 echo
 echo "========================================================="
-echo "Setting up Google Cloud Artifact Registry"
+echo "Enabling Required Google Cloud Platfrom APIs"
+echo "========================================================="
+exe gcloud services enable run.googleapis.com --project $PROJECT_ID
+exe gcloud services enable cloudbuild.googleapis.com --project $PROJECT_ID
+exe gcloud services enable firestore.googleapis.com --project $PROJECT_ID
+
+
+echo
+echo "========================================================="
+echo "Creating Firestore instance"
+echo "========================================================="
+if qexe gcloud firestore databases describe --project $PROJECT_ID; then
+    echo "Skipping. Firestore instance already exists"
+else
+    FIRESTORE_REGION=$REGION
+    if [[ "$REGION" == "us-central1" || "$REGION" == "us-central2" ]]; then
+        # Use multi-region for us-central1 and us-central2
+        echo "Creating Firestore instance in multi-region nam5 instead of $REGION"
+        FIRESTORE_REGION="nam5"
+    fi
+    exe gcloud firestore databases create --location $FIRESTORE_REGION --project $PROJECT_ID
+fi
+# echo
+# echo "========================================================="
+# echo "Enabling Required Firebase APIs"
+# echo "========================================================="
+# exe gcloud services enable firebase.googleapis.com --project $PROJECT_ID
+
+
+# echo
+# echo "========================================================="
+# echo "Adding Firebase to Google Cloud Project"
+# echo "========================================================="
+# if qexe curl https://firebase.googleapis.com/v1beta1/projects/$PROJECT_ID \
+#     -H "Authorization: Bearer $(gcloud auth print-access-token --project $PROJECT_ID)" \
+#     -H "x-goog-user-project: $PROJECT_ID"; then
+#     echo "Skipping. Firebase already added to project $PROJECT_ID"
+# else
+#     echo "Adding Firebase to project $PROJECT_ID"
+#     exe curl -X POST https://firebase.googleapis.com/v1beta1/projects/$PROJECT_ID:addFirebase \
+#         -H "Authorization: Bearer $(gcloud auth print-access-token --project $PROJECT_ID)" \
+#         -H "x-goog-user-project: $PROJECT_ID";
+# fi
+
+
+
+echo
+echo "========================================================="
+echo "Setting Up Google Cloud Artifact Registry"
 echo "========================================================="
 REPO_NAME="demo"
-if gcloud artifacts repositories describe $REPO_NAME --location=$REGION --project=$PROJECT_ID > /dev/null 2>&1; then
+if qexe gcloud artifacts repositories describe $REPO_NAME --location=$REGION --project=$PROJECT_ID; then
     echo "Skipping. Repository with name ${REPO_NAME} in ${REGION} already exists"
 else
     echo "Creating repository ${REPO_NAME} in ${REGION}:"
     exe gcloud artifacts repositories create demo --repository-format=docker --location=$REGION --project=$PROJECT_ID
 fi
-
-# echo
-# echo "========================================================="
-# echo "Preparing Firebase Builder Image"
-# echo "========================================================="
-# FB_IMAGE="$REGION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/firebase"
-# if gcloud artifacts docker images describe $FB_IMAGE > /dev/null 2>&1; then
-#     echo "Skipping. Firebase builder image $FB_IMAGE already exists"
-# else
-#     echo "Building and publishing image $FB_IMAGE"
-#     exe gcloud builds submit --config $SCRIPT_DIR/cloudbuild.firebase.yaml \
-#         --project $PROJECT_ID \
-#         --region $REGION \
-#         --substitutions _REPO_NAME=$REPO_NAME \
-#         .
-# fi
 
 echo
 echo "========================================================="
@@ -88,6 +127,7 @@ echo "Granting the Cloud Run Admin role to the Cloud Build service account:"
 exe gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com \
     --role=roles/run.admin
+
 echo
 echo "Granting the IAM Service Account User role to the Cloud Build service account for the Cloud Run runtime service account:"
 exe gcloud iam service-accounts add-iam-policy-binding \
@@ -95,9 +135,26 @@ exe gcloud iam service-accounts add-iam-policy-binding \
     --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com \
     --role=roles/iam.serviceAccountUser
 
+    # echo
+    # echo "========================================================="
+    # echo "Preparing Firebase Builder Image"
+    # echo "========================================================="
+    # FB_IMAGE="$REGION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/firebase"
+    # if gcloud artifacts docker images describe $FB_IMAGE > /dev/null 2>&1; then
+    #     echo "Skipping. Firebase builder image $FB_IMAGE already exists"
+    # else
+    #     echo "Building and publishing image $FB_IMAGE"
+    #     exe gcloud builds submit --config $SCRIPT_DIR/cloudbuild.firebase.yaml \
+    #         --project $PROJECT_ID \
+    #         --region $REGION \
+    #         --substitutions _REPO_NAME=$REPO_NAME \
+    #         .
+    # fi
+
+
 echo
 echo "========================================================="
-echo "Building and deploying the application to Cloud Run"
+echo "Building and Deploying the Application to Cloud Run"
 echo "========================================================="
 exe gcloud builds submit \
     --project $PROJECT_ID \
